b6bba78e1a641d2ace745231d391034034d0b80a
mikesmall <michael.l.a.small@gmail.com>
2017-05-15 14:48:44 -0400

    Exercise 3 complete

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,14 @@
 puts "What is your name?"
+
+name = gets.chomp
+
+puts "Hello #{name}! May I ask, how old are you?"
+
+age = gets.chomp
+
+year = 2017 - age.to_i
+
+puts "Wow, #{name}, at #{age.to_s}, you must\'ve been born around #{year.to_s} or so!"
+
+
+# Took me a minute to realize I should specify "age.to_i" defining my "year" variable!
