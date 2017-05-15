d6b9bdfb5edaa727879d8797a56c7401e3820800
mikesmall <michael.l.a.small@gmail.com>
2017-05-15 16:51:29 -0400

    Exercise 6.1 completed, including Bad Command option

--- exercise6.rb
+++ exercise6.rb
@@ -2,22 +2,33 @@
 
 # Ask the user for input on what action to take - walk or run. If they walk, the total distance should go up by one, and you should update the user on their total distance traveled as follows:
 
-puts "Are you starting a walk, or a run? Press either \'w\' or \'r\', and hit Enter."
-
 # "Distance from home is 6 km."
 
 # If they run, their total distance should go up by 5. You should keep asking for input - you don't know where you're going until you get there!
 
 travel_distance = 0
 
-walk_or_run = gets.chomp
+loop do
 
-if walk_or_run == "w"
-  travel_distance = travel_distance + 1
+  puts "Walk, run, or go home? Press either \'w\' or \'r\' and hit Enter. To go home, press any key."
 
-elsif walk_or_run == "r"
-  travel_distance = travel_distance + 5
+  what_to_do = gets.chomp
+
+    if what_to_do == "w"
+      travel_distance = travel_distance + 1
+      puts "Distance from home is #{travel_distance.to_s} km."
+
+    elsif what_to_do == "r"
+      travel_distance = travel_distance + 5
+      puts "Distance from home is #{travel_distance.to_s} km."
+
+    elsif what_to_do == "g"
+      puts "Okay, bye!"
+      exit
+
+    else
+      puts "That's not walking, running, OR going home!"
+
+  end
 
 end
-
-puts "Distance from home is #{travel_distance.to_s} km."
