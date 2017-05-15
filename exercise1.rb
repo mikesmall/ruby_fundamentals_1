1720d8ffbd28d7bc0ad4ef7b21c9dc3c460a8972
mikesmall <michael.l.a.small@gmail.com>
2017-05-15 14:05:26 -0400

    Exercise 1 complete

---
+++ exercise1.rb
@@ -0,0 +1,33 @@
+puts 2
+
+puts 3
+
+puts 2 + 3
+
+puts "Betty\'s Pie Shop"
+
+puts "Ada Lovelace lived for #{1852 - 1815} years."
+# Ada Lovelace lived for 37 years.
+
+# Understanding "escape sequences":
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+
+# Boolean operators:
+
+(1 < 3) && (3 < 5)
+# true
+
+(1 > 1) && (2 > 2)
+# false
+
+(1 == 2) || (2 < 3)
+# true
+
+(1 != 2) || (2 < 3)
+# true
+
+(1 == 2) || (2 == 3)
+# false
